838ddf066a0575e409504f74c218d900a020626a
Joseph Lin <44548388+MiaoMiaoKuangFei@users.noreply.github.com>
2020-01-13 10:18:55 +0800

    增加DeveloperDao

---
+++ dao/DeveloperDao.java
@@ -0,0 +1,24 @@
+package dao;
+import entity.Developer;
+
+import java.util.List;
+
+/**
+ * @author ZhutianLin
+ * @version 1.0.0
+ * */
+public interface DeveloperDao {
+    //增加
+    boolean addDeveloper(Developer dev);//增加开发者
+
+    //删除
+    boolean deleteDeveloper(String username);//删除开发者
+
+    //修改
+    boolean updateDeveloper(Developer dev);//修改开发者
+
+    //查找
+    Developer getDeveloper(String name);//获取开发者
+
+
+}
